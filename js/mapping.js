9a1f2916b8a3c5ca5642d8974b18050366ee9004
shujale <hujale.sharmarke@gmail.com>
2018-04-05 12:15:37 +0200

    Initial commit

---
+++ js/mapping.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1amFsZSIsImEiOiJjamZtYXZicDMwb21tMnFvMTQ5YWFrbmFmIn0.buPmKaxPTGEvANf4_qDmCQ';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/kimster/cjf10v6746lt62sliud40d0e8',
+      center: [10.1499006, 56.1172206],
+      pitch: 30,
+        bearing: -30,
+      zoom: 13
+    });
